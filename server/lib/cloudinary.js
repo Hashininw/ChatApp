66a7b9f75a1e657c365ae325c340dcc00362a3ce
Hashininw <153823284+Hashininw@users.noreply.github.com>
2025-06-22 23:03:34 +0530

    first commit

---
+++ server/lib/cloudinary.js
@@ -0,0 +1,9 @@
+import {v2 as cloudinary} from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: "dnyl2gr5n",
+    api_key: "177273213669397",
+    api_secret: "szq-1XGsou_2-nXjCmp-9QciEds",
+});
+
+export default cloudinary;
